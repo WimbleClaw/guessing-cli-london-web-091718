a2abaa1d4ee6d4228d0b07872bacd6baf2326695
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-06 13:41:34 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,9 +6,9 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     guess = gets.strip
     comp_num = rand(1..6).to_s
-      if guess == comp_num
+    if guess == comp_num
       puts "You guessed the correct number!"
-      break
+      #break
     elsif guess == "exit"
       puts "Goodbye!"
       break
